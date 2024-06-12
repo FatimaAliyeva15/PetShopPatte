7fc45a53df038231f1d17fd9b62c7c8356e5ce80
FatimaAliyeva15 <bd7nyulpd@code.edu.az>
2024-06-12 16:25:30 +0400

    Pet Shop Patte front to back

---
+++ PetShop_Patte/PetShop_Patte/Controllers/PetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop_Patte.Controllers
+{
+    public class PetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PetShop_Patte/PetShop_Patte/Controllers/PetDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop_Patte.Controllers
+{
+    public class PetDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
